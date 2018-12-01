de832fe9f48848a0d82d63f6e8a15e33d6524abf
Aldo Caamal <aldocaava@hotmail.com>
2018-11-30 18:15:14 -0600

    Plans Page Carousel
    Added functionality for the carousel on the plans page

--- src/app/plans/plans.component.ts
+++ src/app/plans/plans.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-plans',
@@ -9,16 +9,64 @@ export class PlansComponent implements OnInit {
 
   constructor() { }
 
+  planSelected:string = "Plan One";
 
-  //for the carousel deliver a set of images (local or through Http)
+  //Plan One Images
+  menuOne = [
+    "../../assets/images/joseph-gonzalez-228027-unsplash.png",
+    "../../assets/images/cel-lisboa-60315-unsplash.png",
+    "../../assets/images/taylor-kiser-373479-unsplash.png"
+  ]
+ 
+  //Plan Two Images
+  menuTwo = [
+    "../../assets/images/jannis-brandt-107231-unsplash.png",
+    "../../assets/images/edgar-castrejon-459822-unsplash.png",
+    "../../assets/images/brooke-lark-194252-unsplash.png"
+  ]
+
+  //Plan Three Images
+  menuThree = [
+    "../../assets/images/erik-dungan-1079095-unsplash.png",
+    "../../assets/images/ting-tian-789620-unsplash.png",
+    "../../assets/images/brooke-lark-158019-unsplash.png"
+    
+  ]
+
+  //for the carousel to deliver a set of images (local or through Http)
+  imageSources:any[] = this.menuOne;
   //width can be changed  by changing the containers width
   //the other controls are on the HTML
 
-  imageSources:any[] = [
-    "../../assets/images/cel-lisboa-60315-unsplash.png",
-    "../../assets/images/joseph-gonzalez-228027-unsplash.png",
-    "../../assets/images/taylor-kiser-373479-unsplash.png"
-  ]
+
+
+
+  menuHandler($event: Event){
+   
+    
+    let currentID = $event.target["id"];
+    //check the ID of the plan selected
+    if(currentID === "menuOne"){
+      //set the images array to be the array of images for the menu
+      this.imageSources = this.menuOne;
+      this.planSelected = "Plan One";
+      }
+    else if (currentID === "menuTwo"){
+      console.log("menu two is selected");
+      this.imageSources = this.menuTwo; 
+      this.planSelected = "Plan Two";
+     
+    }
+    else if (currentID === "menuThree"){
+      console.log("menu three is selected");
+      this.imageSources = this.menuThree;
+      this.planSelected = "Plan Three";
+    }
+  
+  }
+
+
+
   ngOnInit() {
   }
 
