9450325083944e2acb2e533591ee97f84b9ccee8
Rovashan <rovashan@gmail.com>
2019-02-26 17:27:24 +0200

    Try get classicMenu

--- src/app/afirestore.service.ts
+++ src/app/afirestore.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { User } from './models/user';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
@@ -11,58 +11,58 @@ export class AfirestoreService {
 
 
   private itemsCollection: any;
-  items:any;
+  items: any;
 
   constructor(
     private firestore: AngularFirestore,
-    ) {
-      /*
-      this.itemsCollection = firestore.collection('subscriptions');
-      this.items = this.itemsCollection.stateChanges();
-    */
-    }
+  ) {
+    /*
+    this.itemsCollection = firestore.collection('subscriptions');
+    this.items = this.itemsCollection.stateChanges();
+  */
+  }
 
-    public collectionChanges(){
-      //checks the changes in the subscriptions collection
-      //only the modified data
-      return this.firestore.collection("subscriptions").stateChanges(["modified"]);
-    }
-    
+  public collectionChanges() {
+    //checks the changes in the subscriptions collection
+    //only the modified data
+    return this.firestore.collection("subscriptions").stateChanges(["modified"]);
+  }
+
   //get current user subscription
-  public getUserSubscription(userSubscription: any){
-     
+  public getUserSubscription(userSubscription: any) {
+
     return this.firestore.collection("subscriptions").doc(userSubscription).valueChanges();
-  }  
+  }
 
   //get the plans
-  public getPlans(){
+  public getPlans() {
     //we need the document ID for the plan reference
     return this.firestore.collection("plans").snapshotChanges();
   }
-  
-  public getSelectedPlan(planId: string){
+
+  public getSelectedPlan(planId: string) {
     return this.firestore.collection("plans").doc(planId).valueChanges();
   }
-  
+
   //add users to the user database
-  public addUser(user: object){
-   return this.firestore.collection("users").add(user);
-   
+  public addUser(user: object) {
+    return this.firestore.collection("users").add(user);
+
   }
 
   //get the plan details
-  public displayPlanData(currentUserID: any){
+  public displayPlanData(currentUserID: any) {
     return this.getUser(currentUserID);
   }
 
   //update user status
-  public updateUserStatus(docId: string){
-    this.firestore.collection("users").doc(docId).update({status: "Active"});
+  public updateUserStatus(docId: string) {
+    this.firestore.collection("users").doc(docId).update({ status: "Active" });
   }
 
 
-  public updateUserSubscription(docId: string, path: any){
-    this.firestore.collection("users").doc(docId).update({subscription: path })
+  public updateUserSubscription(docId: string, path: any) {
+    this.firestore.collection("users").doc(docId).update({ subscription: path })
   }
 
   public getUser(uid: string) {
@@ -71,7 +71,7 @@ export class AfirestoreService {
   }
 
   //create the user subscription
-  public addSubscription(userName: string, userId: string, plan: Object){
+  public addSubscription(userName: string, userId: string, plan: Object) {
     let data = {
       userName: userName,
       userId: userId,
@@ -82,15 +82,15 @@ export class AfirestoreService {
       planCredits: plan["planCredits"],
       address: plan["deliveryAddress"]
     }
-    
+
     //return the function so we can use the observable
     //to update the user subscription
-    return this.firestore.collection("subscriptions").add(data); 
+    return this.firestore.collection("subscriptions").add(data);
   }
 
 
-  public addPaymentReference(userDocId: string, firstName: string, lastName: string, phone: string, 
-                             address: string, building: string, plan: object, subscriptionStartDate: string, subscriptionEndDate: string) {
+  public addPaymentReference(userDocId: string, firstName: string, lastName: string, phone: string,
+    address: string, building: string, plan: object, subscriptionStartDate: string, subscriptionEndDate: string) {
     let data = {
       createdDate: new Date(),
       userId: userDocId,
@@ -100,9 +100,9 @@ export class AfirestoreService {
       userAddress: address,
       userBuilding: building ? building : "Not set",  //if building defined else "Not Set"
       subscribedPlan: plan,
-      subscriptionStartDate: subscriptionStartDate, 
+      subscriptionStartDate: subscriptionStartDate,
       subscriptionEndDate: subscriptionEndDate,
-      paymentStatus: null    
+      paymentStatus: null
     }
     return this.firestore.collection("payments").add(data)
   }
@@ -110,47 +110,51 @@ export class AfirestoreService {
 
   //get the users last subscription
   public renewSubscription(userName: string, userId: string, subscriptionId: string, plan: Object) {
-    
+
     //ref needed to delete past instance of the 
     //user subscription
     let subscriptionRef = this.firestore.collection("subscriptions").doc(subscriptionId);
 
-    
+
     //------------- may need user data for address other details**
-    
+
     //create another subscription
     let data = {
-        userName: userName,
-        userId: userId,
-        planName: plan["planName"],
-        planInitDate: plan["initDate"],
-        planExpDate: plan["expDate"],
-        planId: plan["planId"],
-        planCredits: plan["planCredits"]
-      }
+      userName: userName,
+      userId: userId,
+      planName: plan["planName"],
+      planInitDate: plan["initDate"],
+      planExpDate: plan["expDate"],
+      planId: plan["planId"],
+      planCredits: plan["planCredits"]
+    }
 
+    console.log(data);
+
+    return this.firestore.collection("subscriptions").add(data).then((data) => {
       console.log(data);
 
-      return this.firestore.collection("subscriptions").add(data).then((data) => {
-        console.log(data);
-       
-        this.updateUserStatus(userId);
-        this.updateUserSubscription(userId, data.id);
-        //delete the past subscription doc
-        subscriptionRef.delete();
-      });
-     
+      this.updateUserStatus(userId);
+      this.updateUserSubscription(userId, data.id);
+      //delete the past subscription doc
+      subscriptionRef.delete();
+    });
+
   }
 
   //get the users in order to check the status
-  public checkUser(){
+  public checkUser() {
     return this.firestore.collection("users", ref => ref.orderBy("email", "asc")).snapshotChanges();
   }
 
 
-  public createOrder(orders: object){
+  public createOrder(orders: object) {
     return this.firestore.collection("orders").add(orders);
-  }  
+  }
 
+  //get the menu for landing pages
+  public getClassicMenu() {
+    return this.firestore.doc('landing/classicMenu').valueChanges();
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarouselHandlerService } from "../carousel-handler.service";
 
-import {ViewEncapsulation} from "@angular/core";
+import { ViewEncapsulation } from "@angular/core";
+import { AfirestoreService } from '../afirestore.service';
+import { toArray } from 'rxjs/operators';
 
 
 @Component({
@@ -10,75 +12,81 @@ import {ViewEncapsulation} from "@angular/core";
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-  
+
 export class HomeComponent implements OnInit {
   title = 'app';
+  classicMenu: any;
 
-  constructor(private carouselHandler: CarouselHandlerService) { }
+  constructor(private carouselHandler: CarouselHandlerService,
+    private afirestore: AfirestoreService, ) { }
 
-  
+
   //------------- scroll variables
   startX;
   curDown = false;
   scrollLeft;
   //------------- scroll variables
-    
-  
-  goNext($event: Event){
+
+
+  goNext($event: Event) {
     this.carouselHandler.goNext($event);
-   
+
   }
 
-  goPrev($event: Event){
+  goPrev($event: Event) {
     this.carouselHandler.goPrev($event);
   }
 
   //------------- scroll functions 
-  
-  scrollDrag($event: MouseEvent){
+
+  scrollDrag($event: MouseEvent) {
     $event.preventDefault();
     let el = $event.srcElement.closest(".scrolling-wrapper-flexbox");
-    
+
     const x = $event.pageX - el["offsetLeft"];
     const y = x - this.startX;
 
-    if(this.curDown === true){
+    if (this.curDown === true) {
       el.scrollLeft = this.scrollLeft - y;
-      }else{
+    } else {
       return;
     }
 
-    
-   
-  
- }
+  }
 
-  holdDrag($event: MouseEvent){
+  holdDrag($event: MouseEvent) {
     $event.preventDefault();
     let el = $event.srcElement.closest(".scrolling-wrapper-flexbox");
     this.curDown = true;
     this.startX = $event.pageX - el["offsetLeft"];
     this.scrollLeft = el.scrollLeft;
-    
+
   }
 
-  releaseDrag($event: MouseEvent){
+  releaseDrag($event: MouseEvent) {
     this.curDown = false;
- 
-  
+
+
   }
 
-  leaveDrag($event:MouseEvent){
+  leaveDrag($event: MouseEvent) {
     this.curDown = false;
-  
+
   }
 
 
   //------------- scroll functions 
-  
+
   ngOnInit() {
-  
+    this.getClassicMenu();
   }
-  
- 
+
+  getClassicMenu() {
+    this.afirestore.getClassicMenu().subscribe(items => {
+      console.log('classicMenu: ', items);
+      this.classicMenu = items;
+    })
+  }
+
+
 }
