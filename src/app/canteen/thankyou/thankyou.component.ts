e9e83419080f39e1ca3cf596211781af3a426fc7
drknyt <mutexservice@gmail.com>
2019-03-21 16:52:27 +0200

    Before start check remainingBalance

--- src/app/canteen/thankyou/thankyou.component.ts
+++ src/app/canteen/thankyou/thankyou.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 //get the shopping cart service
-import {ShoppingcartService} from "../../shoppingcart.service";
+import { ShoppingcartService } from "../../shoppingcart.service";
 
 
 @Component({
@@ -19,37 +19,38 @@ export class ThankyouComponent implements OnInit {
 
 
   ngOnInit() {
-  //   if(this.deliveryTime == undefined || this.deliveryTime == null){
-  //     console.log("no time received to split")
-  //   }else{
-  //     if(this.shoppingcart.deliveryTime !== "soon"){
-  //       let timeSplit = this.shoppingcart.deliveryTime.split(':'),
-  //       hours,
-  //       minutes,
-  //       meridian;
-    
-        
-  //     hours = timeSplit[0];
-  //     minutes = timeSplit[1];
-  //     if (hours > 12) {
-  //       meridian = 'pm';
-  //       hours -= 12;
-  //     } else if (hours < 12) {
-  //       meridian = 'am';
-  //       if (hours == 0) {
-  //         hours = 12;
-  //       }
-        
-  //     } else {
-  //       meridian = 'pm';
-  //     }
-  //     this.meridian = meridian;
-    
-  //     }else{
-  //       this.meridian = "";
-  //     }
-    
-  //     }
-  // }
+    //   if(this.deliveryTime == undefined || this.deliveryTime == null){
+    //     console.log("no time received to split")
+    //   }else{
+    //     if(this.shoppingcart.deliveryTime !== "soon"){
+    //       let timeSplit = this.shoppingcart.deliveryTime.split(':'),
+    //       hours,
+    //       minutes,
+    //       meridian;
 
+
+    //     hours = timeSplit[0];
+    //     minutes = timeSplit[1];
+    //     if (hours > 12) {
+    //       meridian = 'pm';
+    //       hours -= 12;
+    //     } else if (hours < 12) {
+    //       meridian = 'am';
+    //       if (hours == 0) {
+    //         hours = 12;
+    //       }
+
+    //     } else {
+    //       meridian = 'pm';
+    //     }
+    //     this.meridian = meridian;
+
+    //     }else{
+    //       this.meridian = "";
+    //     }
+
+    //     }
+    // }
+
+  }
 }
